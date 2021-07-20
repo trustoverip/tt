6fa1d62ce63f41a553b3995eb5c30f243b4abd8d
Daniel Hardman <daniel.hardman@sicpa.com>
2021-07-20 13:21:42 +0200

    Add code repo to TermsWiki
    Signed-off-by: Daniel Hardman <daniel.hardman@sicpa.com>

--- tt/tests/tw_test.py
+++ tt/tests/tw_test.py
@@ -1,10 +1,19 @@
 from ..tw import *
 
-def test_ctwg_parseable():
+
+def test_short_parseable():
     ctwg = TermsWiki('ctwg-terms')
-    assert ctwg.repo_name == 'ctwg-terms'
+    assert ctwg.code_repo_name == 'ctwg-terms'
+    assert ctwg.repo_name == 'ctwg-terms.wiki'
     assert ctwg.github_user == 'trustoverip'
     assert ctwg.tag == '#ctwg'
-    assert ctwg.uri == 'http://github.com/trustoverip/ctwg-terms.wiki.git'
+    assert ctwg.repo_url == 'http://github.com/trustoverip/ctwg-terms.wiki.git'
 
 
+def test_git_code_parseable():
+    ctwg = TermsWiki('git@github.com:foo/someones_terms.git')
+    assert ctwg.code_repo_name == 'someones_terms'
+    assert ctwg.repo_name == 'someones_terms.wiki'
+    assert ctwg.github_user == 'foo'
+    assert ctwg.tag == '#someones'
+    assert ctwg.repo_url == 'http://github.com/foo/someones_terms.wiki.git'
--- tt/tw.py
+++ tt/tw.py
@@ -1,26 +1,47 @@
+import os
 import re
 
 from .tag import normalize
 
-git_repo_pat = re.compile('git@github.com:([^/]+)/(.*?)(?:[.]wiki)?[.]git$')
-https_repo_pat = re.compile('https://github.com/([^/]+)/(.*?)(?:[.]wiki)?[.]git$')
-tag_pat = re.compile('(.*)[_-]terms$', re.I)
+GIT_REPO_PAT = re.compile('git@github.com:([^/]+)/(.*?)(?:[.]wiki)?[.]git$')
+HTTPS_REPO_PAT = re.compile('https://github.com/([^/]+)/(.*?)(?:[.]wiki)?[.]git$')
+TAG_PAT = re.compile('(.*)[_-]terms$', re.I)
+LOCAL_PATH = os.path.normpath(os.path.expanduser('~/.tt/corpus'))
+WIKI_SUFFIX = '.wiki'
 
 
 class TermsWiki:
     def __init__(self, which):
-        m = git_repo_pat.match(which)
+        m = GIT_REPO_PAT.match(which)
         if not m:
-            m = https_repo_pat.match(which)
+            m = HTTPS_REPO_PAT.match(which)
         if m:
             self.github_user = m.group(1)
-            self.repo_name = m.group(2)
+            name = m.group(2)
         else:
             self.github_user = 'trustoverip'
-            self.repo_name = which
-        m = tag_pat.match(self.repo_name)
-        self.tag = normalize(m.group(1) if m else self.repo_name)
+            name = which
+        if name.endswith(WIKI_SUFFIX):
+            self.repo_name = name
+            self.code_repo_name = name[:-1 * WIKI_SUFFIX.len()]
+        else:
+            self.code_repo_name = name
+            self.repo_name = name + WIKI_SUFFIX
+        m = TAG_PAT.match(self.code_repo_name)
+        self.tag = normalize(m.group(1) if m else self.code_repo_name)
 
     @property
-    def uri(self):
-        return 'http://github.com/%s/%s.wiki.git' % (self.github_user, self.repo_name)
+    def code_repo_url(self):
+        return 'http://github.com/%s/%s.git' % (self.github_user, self.code_repo_name)
+
+    @property
+    def repo_url(self):
+        return 'http://github.com/%s/%s.git' % (self.github_user, self.repo_name)
+
+    @property
+    def folder(self):
+        return os.path.join(LOCAL_PATH, self.repo_name)
+
+    @property
+    def exists_locally(self):
+        return os.path.isdir(self.folder)
