f2694f534abecbae5437ee028ac8666fc8cc1ffa
Daniel Hardman <daniel.hardman@sicpa.com>
2021-07-31 19:05:01 +0200

    Fix default css; fix sorting
    Signed-off-by: Daniel Hardman <daniel.hardman@sicpa.com>

--- tt/glossary.py
+++ tt/glossary.py
@@ -1,5 +1,13 @@
+import io
+
 from . import tw
 from . import markdown
+
+
+import os
+with open(os.path.join(os.path.dirname(__file__), 'default.css'), 'rt') as f:
+    DEFAULT_CSS = '  <style>\n%s\n  </style>\n' % f.read()
+del os
 
 
 class Source:
@@ -11,7 +19,7 @@ class Source:
 
 class Glossary:
     def __init__(self, cfg):
-        self.title = cfg.get('title')
+        self._title = cfg.get('title')
         self.css = cfg.get('css')
         sources = cfg.get('sources')
         self._sources = []
@@ -21,7 +29,11 @@ class Glossary:
 
     @property
     def is_standalone_doc(self):
-        return self.title or self.css
+        return self.title or self._css
+
+    @property
+    def title(self):
+        return self._title if self._title else "Glossary"
 
     @property
     def sources(self):
@@ -35,22 +47,24 @@ class Glossary:
                 for page in source.wiki.pages:
                     if page.is_term:
                         self._pages.append(page)
-        self._pages.sort(key=lambda p: page.fragment)
+        self._pages.sort(key=lambda p: p.fragment)
         return self._pages
 
     @property
     def primary_source(self):
         return self._sources[0] if self._sources else None
 
-    def write(self, out):
+    def render(self):
+        out = io.StringIO()
         if self.is_standalone_doc:
             out.write("<html>\n<head>\n")
-            if self.title:
-                out.write("  <title>%s</title>\n" % self.title)
+            out.write("  <title>%s</title>\n" % self.title)
             if self.css:
                 out.write('  <link rel="stylesheet" href="%s">\n' % self.css)
-            out.write("</head>\n<body>\n")
-        out.write("<dl>\n")
+            else:
+                out.write(DEFAULT_CSS)
+            out.write("</head>\n<body>\n<header>%s</header>\n<main>\n" % self.title)
+        out.write("<dl>")
         for page in self.pages:
             out.write('\n<dt id="%s">%s</dt>\n' % (page.fragment, page.term))
             this_def = page.get_section_by_fragment('definition')
@@ -58,7 +72,7 @@ class Glossary:
                 out.write("<dd>%s</dd>\n" % markdown.render_html(this_def.content))
         out.write("</dl>\n")
         if self.is_standalone_doc:
-            out.write("</body>\n</html>\n")
-
-
+            out.write("</main>\n</body>\n</html>\n")
+        out.seek(0)
+        return out.read()
 
--- tt/tests/glossary_test.py
+++ tt/tests/glossary_test.py
@@ -13,11 +13,11 @@ SAMPLE_GLOSSARY_CFG = json.loads("""{
 }""")
 
 def test_ultra_simple_write():
-    s = Glossary(SAMPLE_GLOSSARY_CFG)
-    txt = io.StringIO()
-    s.write(txt)
-    txt.seek(0)
-    txt = txt.read()
+    g = Glossary(SAMPLE_GLOSSARY_CFG)
+    txt = g.render()
+    with open("/Users/dhardman/Desktop/x.html", "wt") as f:
+        f.write(txt)
+
     expected = ['<html>', '<head>', '</head>', '<body>',
                 '<title>My Simple Glossary</title>',
                 'wormhole',
@@ -27,10 +27,6 @@ def test_ultra_simple_write():
     ]
     missing = [x for x in expected if x not in txt]
     assert not bool(missing)
-
-
-if __name__ == '__main__':
-    test_ultra_simple_write()
 
 
 
